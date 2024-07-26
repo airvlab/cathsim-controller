b3c6f4546c5cbe0588ae98ae81c20b76318f560e
Cloud <lmy-lee1@outlook.com>
2024-07-26 17:03:03 +0100

    update link console and realenv--not finished

--- src/console/console.py
+++ src/console/console.py
@@ -1,11 +1,12 @@
 import pygame
 from pygame import joystick, event, time, JOYAXISMOTION, JOYBALLMOTION, JOYBUTTONDOWN, JOYBUTTONUP, JOYHATMOTION, JOYDEVICEADDED, JOYDEVICEREMOVED, QUIT
-
+from src.controller import RealEnv
 
 
 class Console:
 
     def __init__(self) -> None:
+        pygame.init()
         # Init the  Joystick module
         if not joystick.get_init():
             joystick.init()
@@ -22,6 +23,7 @@ class Console:
         if joystick.get_init():
             # uninstall the module.
             joystick.quit()
+        pygame.quit()
 
     def track_axis(self, id) -> None:
         done = False
@@ -38,24 +40,55 @@ class Console:
                     (left_control,
                      right_control) = (self.joystick_obj[id].get_axis(i)
                                        for i in Axis_id)
-                    if abs(left_control-0)>1e-1 or abs(right_control-0)>1e-1:
+                    if abs(left_control - 0) > 1e-1 or abs(right_control -
+                                                           0) > 1e-1:
                         print(left_control, right_control)
                 if events.type == JOYBUTTONDOWN:
                     done = True
 
     def get_info(self) -> dict:
-        joysticks = []
+        joystick_items = []
         for i in range(self._num_joystick):
-            joysticks.append(dict(id=i, name=self.joystick_obj[i].get_name()))
-        return dict(num=self._num_joystick, joysticks=joysticks)
+            joystick_items.append({
+                "id": i,
+                "name": self.joystick_obj[i].get_name()
+            })
+        return dict(num=self._num_joystick, joystick_items=joystick_items)
 
+
+class Link_console_controller:
+    def __init__(self) -> None:
+        self.env = RealEnv(width=80, height=80)
+        self.env.reset()
+        self.console = Console()
+        console_info = self.console.get_info()
+        print(
+            f"You have {console_info.get(num) } consoles; {console_info.get(joystick_items)}"
+        )
+        input_user = input("which console are you gonna to use(default 0):")
+        if not input_user.strip():
+            id = 0
+        try:
+            id = int(input_user)
+        except ValueError:
+            print("Not a valid integer value, will be set as default 0")
+            id = 0
+        if id < 0 or id >= console_info.get(num):
+            print("Value out of ranges, will be set as default 0")
+            id = 0
+
+    def begin(self):
+        self.console.track_axis(id)
+
+    def deal_data(self, translation, rotation, end):
+        self.cum_translation, self.cum_rotation += translation / 15., rotation / 15.
+        if end or self.cum_translation > 1.0 or self.cum_rotation > 1.0 or self.cum_translation < -1.0 or self.cum_rotation < -1.0:
+            action = [self.cum_translation, self.cum_rotation]
+            self.env.step(action)
+            self.cum_translation, self.cum_rotation = 0, 0
 
 if __name__ == "__main__":
-    pygame.init()
     # Used to manage how fast the screen updates.
-    clock = time.Clock()
-
+    # clock = time.Clock()
     my_console = Console()
-    print(my_console.get_info())
-    my_console.track_axis(0)
-    pygame.quit()
+    # print(my_console.get_info())
