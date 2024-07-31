2de8d4ecdd0a959533477652eb0cfec8dd550670
Tudor Jianu <tudorjnu@gmail.com>
2024-07-31 16:51:37 +0100

    refactor

--- src/controller/wrapper/camera.py
+++ src/controller/wrapper/camera.py
@@ -1,75 +1,67 @@
-import pyrealsense2 as rs
-import numpy as np
 import cv2
-import matplotlib.pyplot as plt
-
+import numpy as np
+import pyrealsense2 as rs
 
 
 class Camera:
-    _pipeline = None
-    _image = None
-
-    def __init__(self):
-        # Configure depth and color streams
-        self._pipeline = rs.pipeline()
+    def __init__(self, width: int = 640, height: int = 482, fps: int = 30):
         config = rs.config()
+        config.enable_stream(rs.stream.color, width, height, rs.format.rgb8, fps)
 
-        # Get device product line for setting a supporting resolution
-        pipeline_wrapper = rs.pipeline_wrapper(self._pipeline)
-        pipeline_profile = config.resolve(pipeline_wrapper)
-        device = pipeline_profile.get_device()
-        device_product_line = str(device.get_info(rs.camera_info.product_line))
+        self._pipeline = rs.pipeline()
+        self._pipeline_wrapper = rs.pipeline_wrapper(self._pipeline)
+        self._pipeline_profile = config.resolve(self._pipeline_wrapper)
+        self._device = self._pipeline_profile.get_device()
+        self._profiles = self.get_profiles(self._device)
+        self._validate_profile(fps, width, height, self._profiles)
 
-        found_rgb = False
-        for s in device.sensors:
-            if s.get_info(rs.camera_info.name) == "RGB Camera":
-                found_rgb = True
-                break
-        if not found_rgb:
-            print("The demo requires Depth camera with Color sensor")
-            exit(0)
+        if not self._has_rgb_sensor(self._device):
+            raise Exception("The demo requires Depth camera with Color sensor")
 
-        # config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-        config.enable_stream(rs.stream.color, 1280 720, rs.format.bgr8, 15)
-
-        # Start streaming
         self._pipeline.start(config)
-        
+
+    @staticmethod
+    def _validate_profile(fps, width, height, profiles):
+        for profile in profiles:
+            if profile[0] == fps and profile[1] == width and profile[2] == height:
+                return True
+        raise Exception(f"Invalid resolution: {width}x{height} @ {fps}fps")
+
+    @staticmethod
+    def _has_rgb_sensor(device):
+        for sensor in device.sensors:
+            if sensor.get_info(rs.camera_info.name) == "RGB Camera":
+                return True
+        return False
 
     def __del__(self):
-        # Stop streaming
         self._pipeline.stop()
 
-    def get_image(self, width, height):
+    def get_image(self):
         # Wait for a coherent pair of frames: depth and color
         frames = self._pipeline.wait_for_frames()
         color_frame = frames.get_color_frame()
-        # Convert images to numpy arrays
-        color_image = np.asanyarray(color_frame.get_data())
-        # color_colormap_dim = color_image.shape
-        self._image = color_image
-        return self._image
-    # def get_video(self, width, height):
-    #     # 创建VideoWriter对象，定义视频的输出文件、编码器、帧率和分辨率
-    #     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    #     out = cv2.VideoWriter('output.avi', fourcc, 30.0, (1024, 1024))
-    #     while True:
-    #         # Wait for a coherent pair of frames: depth and color
-    #         frames = self._pipeline.wait_for_frames()
-    #         color_frame = frames.get_color_frame()
-    #         # Convert images to numpy arrays
-    #         color_image = np.asanyarray(color_frame.get_data())
-    #         out.write(color_image)
-    #         # 按下 'q' 键退出
-    #         if cv2.waitKey(1) & 0xFF == ord('q'):
-    #             break
-    #     out.release()
-   
+        image = np.asanyarray(color_frame.get_data())
+        return image
 
-if __name__ == "main":
-    test = Camera()
-    img = test.get_image()
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
-    cv2.destroyAllWindows()
+    def get_profiles(self, device):
+        profiles = []
+        rgb_sensor = None
+        for sensor in device.sensors:
+            if sensor.get_info(rs.camera_info.name) == "RGB Camera":
+                rgb_sensor = sensor
 
+        for profile in rgb_sensor.profiles:
+            profile = profile.as_video_stream_profile()
+            profiles.append((profile.fps(), profile.width(), profile.height()))
+
+        return profiles
+
+
+if __name__ == "__main__":
+    camera = Camera(width=642, height=480)
+    while True:
+        image = camera.get_image()
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        cv2.imshow("RealSense", image)
+        cv2.waitKey(1)
