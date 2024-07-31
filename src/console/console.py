bd0713db27f8c95e134f35fbc00172379c5f4e8f
Tudor Jianu <tudorjnu@gmail.com>
2024-07-31 15:21:09 +0100

    up

--- src/console/console.py
+++ src/console/console.py
@@ -1,19 +1,15 @@
 import pygame
-from pygame import joystick, event, time, JOYAXISMOTION, JOYBALLMOTION, JOYBUTTONDOWN, JOYBUTTONUP, JOYHATMOTION, JOYDEVICEADDED, JOYDEVICEREMOVED, QUIT
-from src.controller import RealEnv
+from pygame import joystick
 
 
 class Console:
-
     def __init__(self) -> None:
         pygame.init()
         # Init the  Joystick module
         if not joystick.get_init():
             joystick.init()
         self._num_joystick = joystick.get_count()
-        self.joystick_obj = [
-            joystick.Joystick(i) for i in range(self._num_joystick)
-        ]
+        self.joystick_obj = [joystick.Joystick(i) for i in range(self._num_joystick)]
 
     def __del__(self) -> None:
         for i in range(self._num_joystick):
@@ -24,9 +20,10 @@ class Console:
             # uninstall the module.
             joystick.quit()
         pygame.quit()
+
     def choose_joystick(self):
         print(
-        f"You have {self._num_joystick } consoles; {self.get_info().joystick_items()}"
+            f"You have {self._num_joystick } consoles; {self.get_info().joystick_items()}"
         )
         input_user = input("which console are you gonna to use(default 0):")
         if not input_user.strip():
@@ -39,18 +36,16 @@ class Console:
         if self.track_id < 0 or self.track_id >= self._num_joystick:
             print("Value out of ranges, will be set as default 0")
             self.track_id = 0
+
     def get_info(self) -> dict:
         joystick_items = []
         for i in range(self._num_joystick):
-            joystick_items.append({
-                "id": i,
-                "name": self.joystick_obj[i].get_name()
-            })
+            joystick_items.append({"id": i, "name": self.joystick_obj[i].get_name()})
         return dict(num=self._num_joystick, joystick_items=joystick_items)
 
-    
     def track_axis(self):
         pass
+
 
 if __name__ == "__main__":
     my_console = Console()
