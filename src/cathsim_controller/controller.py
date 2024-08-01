5d361475a020d70c4a13ab5e03b6a638582ce178
Tudor Jianu <tudorjnu@gmail.com>
2024-08-01 10:55:36 +0100

    up

--- src/cathsim_controller/controller.py
+++ src/cathsim_controller/controller.py
@@ -4,22 +4,23 @@ import serial
 
 
 class Controller:
-    _port = None
-    _ser = None
-    _curPosition = 0  # each step is 5mm ,so every move need to times 5
-    _rightBound = 30000  # the right bound of step position is 0
-    _leftBound = -30000  # the left bound of step position is -6000
+    _right_bound = 30000  # the right bound of step position is 0
+    _left_bound = -30000  # the left bound of step position is -6000
 
-    def __init__(self, port):
+    def __init__(
+        self,
+        port: str = "/dev/ttyUSB0",
+        translation_scale_factor: float = 0.1,
+        rotation_scale_factor: float = 0.1,
+    ):
         self._port = port
-        # open usb0 and set brut 115200
-        self._ser = serial.Serial(self._port, 115200)
+        self._serial = serial.Serial(self._port, baudrate=115200)
+        self._current_position = 0
 
     def __del__(self):
-        # for next time have the same global position
         self._move_to_global_position(0, 0)
         sleep(1)
-        self._ser.close()
+        self._serial.close()
 
     def send(self, enable, motor1, motor2, motor3, motor4, relative):
         data = bytearray(19)
@@ -48,14 +49,14 @@ class Controller:
         data[15] = (motor4 & 0x00FF0000) >> 16
         data[16] = (motor4 & 0x0000FF00) >> 8
         data[17] = motor4 & 0x000000FF
-        self._ser.flush()
-        self._ser.write(data)
-        sleep(1)
-        self._ser.flush()
+        self._serial.flush()
+        self._serial.write(data)
+        sleep(0.5)
+        self._serial.flush()
 
     def _check_bound(self, check_position):
         assert (
-            self._leftBound <= check_position <= self._rightBound
+            self._left_bound <= check_position <= self._right_bound
         ), "The move out of range, and be cancelled"
         return True
 
@@ -75,27 +76,27 @@ class Controller:
         ), f"Got rotation {rotation}, expected in range (-1, 1)"
         return True
 
-    def get_inf(self):
+    def get_info(self):
         return (
-            self._curPosition / 100.0,
-            self._rightBound / 100.0,
-            self._leftBound / 100.0,
+            self._current_position / 100.0,
+            self._right_bound / 100.0,
+            self._left_bound / 100.0,
         )
 
-    def _move_to_relative_position(self, translation, rotation):
+    def _move_to_relative_position(self, translation: float, rotation: float):
         # motor3B, motor4B should be in range(-1,1)
         motor3_scale_factor = 500  # 5 mm; 800 step one rotation -8mm
         motor4_scale_factor = 200  # 90 degree; 800 step 360 degree
 
         motor3 = int(translation * float(motor3_scale_factor))
         motor4 = int(rotation * float(motor4_scale_factor))
-        expectPosition = self._curPosition + motor3
+        expectPosition = self._current_position + motor3
         self._check_bound(check_position=expectPosition)
         self.send(
             enable=True, motor1=0, motor2=0, motor3=motor3, motor4=motor4, relative=True
         )
 
-    def _move_to_global_position(self, translation, rotation):
+    def _move_to_global_position(self, translation: float, rotation: float):
         # motor3B, motor4B should be in range(0,1)
         # change range from(-1,1) to range (0,1)
         # translation = (translation + 1.0) / 2.0
@@ -115,7 +116,7 @@ class Controller:
             relative=False,
         )
 
-    def move(self, translation, rotation, relative=True):
+    def move(self, translation: float, rotation: float, relative=True):
         self._check_type_range(translation, rotation)
 
         if relative:
