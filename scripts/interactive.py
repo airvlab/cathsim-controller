091c605d1d400f2d6db6866f67d1d8ee859767d8
Tudor Jianu <jianu.tudor9@gmail.com>
2024-08-03 09:23:34 +0100

    refactor

--- scripts/interactive.py
+++ scripts/interactive.py
@@ -1,38 +1,41 @@
 from pathlib import Path
 
+import cv2
 from cathsim_controller.joystick import Joystick
 from cathsim_controller.real_env import RealEnv
-from cathsim_controller.utils import videos_path
-import cv2
+
+from .utils import videos_path as VIDEOS_PATH
 
 FPS = 15
-FILE_PATH = videos_path
-FILE_PATH.mkdir(exist_ok=True)
+WIDTH = 1280
+HEIGHT = 720
+
+VIDEOS_PATH.mkdir(exist_ok=True)
 FILE_NAME = "interactive.mp4"
+
 joystick = Joystick(
     left_stick_vertical_axis=1,
     right_stick_horizontal_axis=2,
 )
-env = RealEnv(image_width=1280, image_height=720, fps=FPS)
+
+env = RealEnv(image_width=WIDTH, image_height=HEIGHT, fps=FPS)
 env.reset()
 
 frames_directory = Path("frames")
 if not frames_directory.exists():
     frames_directory.mkdir()
 
-fourcc=cv2.VideoWriter_fourcc(*'mp4v')
-out=cv2.VideoWriter(FILE_PATH/FILE_NAME,fourcc,FPS,(1280,720))
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+out = cv2.VideoWriter(VIDEOS_PATH / FILE_NAME, fourcc, FPS, (WIDTH, HEIGHT))
 
 try:
     while True:
         action = joystick.get_input()
         observation, _, _, _, _ = env.step(action)
-        if observation is not None:
-                out.write(observation)
-                cv2.imshow('Image',observation)
-                # if cv2.waitKey(1) & 0xFF == ord('q'):
-                #     break
-        # pygame.time.wait(1000)
+        image = observation["pixels"]
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        out.write(image)
+        cv2.imshow("Image", image)
 except KeyboardInterrupt:
     print("Exiting...")
-    # cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
