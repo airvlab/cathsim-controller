071beb8c5ed305a073c5f6f0d29c6d238733cd32
Tudor Jianu <tudorjnu@gmail.com>
2024-08-09 10:56:20 +0100

    fix camera

--- src/cathsim_controller/camera.py
+++ src/cathsim_controller/camera.py
@@ -4,32 +4,45 @@ import pyrealsense2 as rs
 
 
 class Camera:
+    profiles = {
+        (6, 424, 240),
+        (6, 640, 480),
+        (6, 1280, 720),
+        (8, 1920, 1080),
+        (10, 1280, 720),
+        (15, 424, 240),
+        (15, 640, 480),
+        (15, 1280, 720),
+        (30, 424, 240),
+        (30, 640, 480),
+        (60, 424, 240),
+    }
+
     def __init__(
-        self, width: int = 640, height: int = 480, fps: int = 30, square=False
+        self, width: int = 640, height: int = 480, fps: int = 30, use_square=False
     ):
+        self._validate_profile(fps, width, height)
+        self.use_square = use_square
+
         config = rs.config()
         config.enable_stream(rs.stream.color, width, height, rs.format.rgb8, fps)
-
-        self.square = square
 
         self._pipeline = rs.pipeline()
         self._pipeline_wrapper = rs.pipeline_wrapper(self._pipeline)
         self._pipeline_profile = config.resolve(self._pipeline_wrapper)
         self._device = self._pipeline_profile.get_device()
         self._profiles = self.get_profiles(self._device)
-        self._validate_profile(fps, width, height, self._profiles)
 
         if not self._has_rgb_sensor(self._device):
             raise Exception("The demo requires Depth camera with Color sensor")
 
         self._pipeline.start(config)
 
-    @staticmethod
-    def _validate_profile(fps, width, height, profiles):
-        for profile in profiles:
+    def _validate_profile(self, fps, width, height):
+        for profile in self.profiles:
             if profile[0] == fps and profile[1] == width and profile[2] == height:
                 return True
-        raise Exception(f"Invalid resolution: {width}x{height} @ {fps}fps")
+        raise Exception(f"Invalid resolution: {width}x{height}@{fps}fps")
 
     @staticmethod
     def _has_rgb_sensor(device):
@@ -39,11 +52,11 @@ class Camera:
         return False
 
     def __del__(self):
-        self._pipeline.stop()
+        if hasattr(self, "_pipeline"):
+            self._pipeline.stop()
 
     def make_square(self, image):
         height, width, _ = image.shape
-        # crop the image to a square
         image = image[
             (height - min(height, width)) // 2 : (height + min(height, width)) // 2,
             (width - min(height, width)) // 2 : (width + min(height, width)) // 2,
@@ -54,12 +67,12 @@ class Camera:
         frames = self._pipeline.wait_for_frames()
         color_frame = frames.get_color_frame()
         image = np.asanyarray(color_frame.get_data())
-        if self.square:
+        if self.use_square:
             image = self.make_square(image)
         return image
 
     def get_profiles(self, device):
-        profiles = []
+        profiles = set()
         rgb_sensor = None
         for sensor in device.sensors:
             if sensor.get_info(rs.camera_info.name) == "RGB Camera":
@@ -67,14 +80,13 @@ class Camera:
 
         for profile in rgb_sensor.profiles:
             profile = profile.as_video_stream_profile()
-            profiles.append((profile.fps(), profile.width(), profile.height()))
+            profiles.add((profile.fps(), profile.width(), profile.height()))
 
         return profiles
 
 
 if __name__ == "__main__":
-    camera = Camera(width=1920, height=1080, fps=8, square=True)
-    # print(camera._profiles)
+    camera = Camera(width=1920, height=1080, fps=8, use_square=True)
     while True:
         image = camera.get_image()
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
